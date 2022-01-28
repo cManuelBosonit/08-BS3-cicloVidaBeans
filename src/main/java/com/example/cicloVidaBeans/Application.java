4a67898e21aacbbcc2298cad4e2073ac690cc5e8
Manuel Hernández <carlosmanuel.hernandez@bosonit.com>
2022-01-28 10:10:56 +0100

    Ejercicio Ok

---
+++ src/main/java/com/example/cicloVidaBeans/Application.java
@@ -0,0 +1,40 @@
+package com.example.cicloVidaBeans;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+
+
+	@Bean
+	CommandLineRunner ejecutame(){
+		return p ->
+		{
+			System.out.println("Hola desde la clase secundaria");
+		};
+	}
+
+	@Bean
+	CommandLineRunner ejecutame2(){
+		return p ->
+		{
+			System.out.println("Soy la tercera clase");
+		};
+	}
+
+	@PostConstruct
+	public void llamada(){
+		System.out.println("Hola desde la clase inicial");
+	}
+
+}
